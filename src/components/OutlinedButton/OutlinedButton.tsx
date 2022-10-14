ff3c61271144eb78e294e9d41fe694902a243086
Brian Holt <brianholt@brians-mbp-2.home>
2022-10-14 14:43:23 -0700

    outlined btn

---
+++ src/components/OutlinedButton/OutlinedButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MuiButton from '../MuiButton';
+import { ButtonProps as MuiButtonProps} from '@mui/material';
+
+type ButtonBaseProps = Pick<MuiButtonProps, 
+    "disabled" |
+    "size" | 
+    "color" | 
+    "children" | 
+    "fullWidth"
+>;
+
+type ButtonProps = ButtonBaseProps & {
+    label: string
+};
+const OutlinedButton: React.FC<ButtonProps> = ({ label }) => (
+   <MuiButton label={label} variant='outlined' />
+);
